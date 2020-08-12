18d6a7c0e616263df06227e708e6859a78c446b8
yeongjin <syeongjin144@gmail.com>
2020-08-12 12:42:39 +0900

    commit

--- fabfile.py
+++ fabfile.py
@@ -26,7 +26,7 @@ env.hosts = [
 ]
 env.use_ssh_config = True
 # ssh로 접속시 필요한 개인키의 상대경로
-env.key_filename = '../kfq_busan.ppk'
+env.key_filename = '../kfq_busan.pem'
 # 리눅스에 세팅될 원격 위치
 # /home/ubuntu/awsdeploy
 project_folder = '/home/{}/{}'.format(env.user, PROJECT_NAME)
